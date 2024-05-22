d031d5a216f1b12133f66edb6cabef7f51481ef3
Blaine <blainesolomon@users.noreply.github.com>
2024-05-22 03:47:24 -0700

    Initial Commit

---
+++ SquadArticleDemo/SquadArticleDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SquadArticleDemoApp.swift
+//  SquadArticleDemo
+//
+//  Created by Blaine on 5/22/24.
+//
+
+import SwiftUI
+
+@main
+struct SquadArticleDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
